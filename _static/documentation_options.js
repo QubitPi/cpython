b4cae718b18f9a4d6b2e2893d51d48af97dae411
QubitPi <QubitPi@users.noreply.github.com>
2025-05-17 03:27:24 +0000

    deploy: 6cf357c577d43b2558599297f06859fe41bb005b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.12.0a4',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.15.0a0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
